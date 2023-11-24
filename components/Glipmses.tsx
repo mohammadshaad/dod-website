2de2a46ecb2420ec174d45e91a2beb93df3ccf69
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-11-25 02:27:35 +0530

    :white_check_mark: Added gallery section :sparkles:

--- components/Glipmses.tsx
+++ components/Glipmses.tsx
@@ -8,7 +8,7 @@ import Gallery from './Gallery'
 
 function Glipmses() {
     return (
-        <div className='w-full'>
+        <div className='w-full py-10 md:py-20'>
             <div className='below-bg flex flex-col items-center justify-center w-full h-full bg-[#260441]'>
                 <div className='flex items-center justify-center w-full'>
                     <div className='md:px-24 md:py-2 uppercase underline font-museoModernoMedium text-sm md:text-xl  text-white/50 flex-shrink-0 underline-offset-4'>
@@ -16,7 +16,7 @@ function Glipmses() {
                     </div>
                 </div>
 
-                <div className='w-full mt-10 '>
+                <div className='w-full  md:mt-10 rounded-3xl'>
                     <Gallery />
                 </div>
             </div>
