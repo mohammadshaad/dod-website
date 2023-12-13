cd7767b45a699cc2b89ae11ad398ec71c880c760
Mohammad Shaad <mohammadshaadshaikh@gmail.com>
2023-12-13 12:09:05 +0530

    feat: improved seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,40 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+
+      {/* Meta Tags */}
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="FYIVITC brings you another thrilling Hackathon" />
+      {/* Add other meta tags, title, etc. here */}
+
+      {/* Open Graph Meta Tags (for social media sharing) */}
+      <meta property="og:title" content="DUNGEON OF DEVELOPERS" />
+      <meta property="og:description" content="FYIVITC brings you another thrilling Hackathon" />
+      <meta property="og:image" content="https://github.com/mohammadshaad/dod-website/blob/main/public/images/metatag.png?raw=true" />
+      <meta property="og:url" content="https://www.dungeonofdevs.tech" />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter Meta Tags (for Twitter Card) */}
+      <meta name="twitter:card" content="https://github.com/mohammadshaad/dod-website/blob/main/public/images/metatag.png?raw=true" />
+      <meta name="twitter:title" content="DUNGEON OF DEVELOPERS" />
+      <meta name="twitter:description" content="FYIVITC brings you another thrilling Hackathon" />
+      <meta name="twitter:image" content="https://github.com/mohammadshaad/dod-website/blob/main/public/images/metatag.png?raw=true" />
+
+      {/* Favicon */}
+      <link rel="icon" href="/favicon.ico" />
+
+      {/* Apple Touch Icon */}
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+      {/* CSS Reset or Normalize.css */}
+      {/* <link rel="stylesheet" href="path-to-reset-or-normalize.css" /> */}
+
+      {/* Your custom stylesheets or fonts */}
+      {/* <link rel="stylesheet" href="path-to-your-custom-styles.css" /> */}
+
+      {/* Title */}
+      <title>DUNGEON OF DEVELOPERS</title>
       <body>
         <Main />
         <NextScript />
